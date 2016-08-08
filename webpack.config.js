98eb899c1fa982046c84265cec737c35e28b8a3c
Akshay Anand <aks9800@gmail.com>
2016-08-08 15:55:31 -0400

    image compression added

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,14 @@ var config = {
       	test: /\.scss$/,
       	loader: ExtractTextPlugin.extract('css!sass')
       	//loaders: ['style', 'css', 'sass']
-      }
+      },
+      {
+      	test: /\.(jpe?g|png|gif|svg)$/i,
+	    loaders: [
+	        'file?hash=sha512&digest=hex&name=[hash].[ext]',
+	        'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+	    ]
+	   }
     ]
   }, 
   //plugins
