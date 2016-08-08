edcd7fc8eb437e46106c0853a18136e2bf41d766
Akshay Anand <aks9800@gmail.com>
2016-08-08 13:41:18 -0400

    router added

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import {render} from 'react-dom';
+import { Router, Route, Link } from 'react-router'
 
 import AwesomeComponent from './AwesomeComponent.jsx';
 import MyComponent from './MyComponent/MyComponent.jsx'
 
 
-class App extends React.Component {
+class MainLayout extends React.Component {
   render () {
     return( 
     	<div>
@@ -17,4 +18,8 @@ class App extends React.Component {
   }
 }
 
-render(<App/>, document.getElementById('app'));
+render((
+	<Router>
+    	<Route path="/" component={MainLayout} />
+    </Router>
+), document.getElementById('app'));
