9d10e0d8a900b8de0c206462625ee91f3876c665
Akshay Anand <aks9800@gmail.com>
2016-08-07 20:45:13 -0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var webpack = require('webpack');
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+
+var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
+var APP_DIR = path.resolve(__dirname, 'src/client/app');
+
+var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: APP_DIR + '/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+
+var config = {
+  entry: APP_DIR + '/index.jsx',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  //modules
+  module : {
+    loaders : [
+      {
+        test : /\.jsx?/,
+        include : APP_DIR,
+        loader : 'babel'
+      }
+    ]
+  }, 
+  //plugins
+  plugins: [HTMLWebpackPluginConfig]
+};
+
+module.exports = config;
